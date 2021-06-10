1464e604f72ba7a52cf1c13c7ac1a80aa00265fe
evgeniy <not-found@inbox.ru>
2021-06-10 22:29:48 +0300

    Компоненту FilmCatalog добавлен стейт

--- project/src/components/film-catalog/film-catalog.jsx
+++ project/src/components/film-catalog/film-catalog.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import filmProp from '../film/film.prop';
 import SmallFilmCard from '../small-film-card/small-film-card';
 
 function FilmCatalog({films}) {
+  const [activeFilmCard, setactiveFilmCard] = useState({});
+
+
   return (
     <div className="catalog__films-list">
-      {films.map((film) => <SmallFilmCard film={film} key={film.id}/>)}
+      {films.map((film) => (
+        <SmallFilmCard
+          film={film}
+          key={film.id}
+          hoverHandler={setactiveFilmCard}
+        />
+      ))}
     </div>
   );
 }
--- project/src/components/small-film-card/small-film-card.jsx
+++ project/src/components/small-film-card/small-film-card.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import filmProp from '../film/film.prop';
+import PropTypes from 'prop-types';
 
-function SmallFilmCard({film}) {
+
+function SmallFilmCard({film, hoverHandler}) {
   const {title, cover} = film;
 
 
   return (
-    <article className="small-film-card catalog__films-card">
+    <article
+      className="small-film-card catalog__films-card"
+      onMouseEnter={() => {hoverHandler(film);}}
+      onMouseLeave={() => {hoverHandler({});}}
+    >
       <div className="small-film-card__image">
         <img src={cover} alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175" />
       </div>
@@ -19,5 +25,6 @@ function SmallFilmCard({film}) {
 
 SmallFilmCard.propTypes = {
   film: filmProp,
+  hoverHandler: PropTypes.func.isRequired,
 };
 export default SmallFilmCard;
