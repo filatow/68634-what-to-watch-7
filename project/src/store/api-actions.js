9cf28bec428c2ac9645321377668575db713be78
evgeniy <not-found@inbox.ru>
2021-07-24 18:44:12 +0300

    Добавлены тесты для асинхронных экшенов

--- project/src/store/api-actions.js
+++ project/src/store/api-actions.js
@@ -18,7 +18,7 @@ import {adaptFilmToClient} from '../utils';
 
 export const fetchFilmList = () => (dispatch, _getState, api) => {
   dispatch(startLoading(LoadedData.FILMS));
-  api.get(APIRoute.FILMS)
+  return api.get(APIRoute.FILMS)
     .then(({data}) => dispatch(loadFilms(
       data.map((film) => adaptFilmToClient(film)),
     )))
@@ -28,7 +28,7 @@ export const fetchFilmList = () => (dispatch, _getState, api) => {
 
 export const fetchPromotedFilm = () => (dispatch, _getState, api) => {
   dispatch(startLoading(LoadedData.PROMOTED_FILM));
-  api.get(APIRoute.PROMO)
+  return api.get(APIRoute.PROMO)
     .then(({data}) => dispatch(loadPromotedFilm(adaptFilmToClient(data))))
     .catch(() => {})
     .finally(() => dispatch(stopLoading(LoadedData.PROMOTED_FILM)));
@@ -36,7 +36,7 @@ export const fetchPromotedFilm = () => (dispatch, _getState, api) => {
 
 export const fetchCurrentFilm = (filmId) => (dispatch, _getState, api) => {
   dispatch(startLoading(LoadedData.CURRENT_FILM));
-  api.get(`${APIRoute.FILMS}/${filmId}`)
+  return api.get(`${APIRoute.FILMS}/${filmId}`)
     .then(({data}) => dispatch(loadCurrentFilm(adaptFilmToClient(data))))
     .catch(() => {})
     .finally(() => dispatch(stopLoading(LoadedData.CURRENT_FILM)));
@@ -44,7 +44,7 @@ export const fetchCurrentFilm = (filmId) => (dispatch, _getState, api) => {
 
 export const fetchSimilarFilms = (filmId) => (dispatch, _getState, api) => {
   dispatch(startLoading(LoadedData.SIMILAR_FILMS));
-  api.get(`${APIRoute.FILMS}/${filmId}${APIRoute.SIMILAR}`)
+  return api.get(`${APIRoute.FILMS}/${filmId}${APIRoute.SIMILAR}`)
     .then(({data}) => dispatch(loadSimilarFilms(
       data.map((film) => adaptFilmToClient(film)),
     )))
@@ -52,9 +52,9 @@ export const fetchSimilarFilms = (filmId) => (dispatch, _getState, api) => {
     .finally(() => dispatch(stopLoading(LoadedData.SIMILAR_FILMS)));
 };
 
-export const fetchFavoriteFilms = (filmId) => (dispatch, _getState, api) => {
+export const fetchFavoriteFilms = () => (dispatch, _getState, api) => {
   dispatch(startLoading(LoadedData.FAVORITE_FILMS));
-  api.get(APIRoute.FAVORITE)
+  return api.get(APIRoute.FAVORITE)
     .then(({data}) => dispatch(loadFavoriteFilms(
       data.map((film) => adaptFilmToClient(film)),
     )))
@@ -62,51 +62,45 @@ export const fetchFavoriteFilms = (filmId) => (dispatch, _getState, api) => {
     .finally(() => dispatch(stopLoading(LoadedData.FAVORITE_FILMS)));
 };
 
-
 export const fetchFilmComments = (filmId) => (dispatch, _getState, api) => {
   dispatch(startLoading(LoadedData.FILM_COMMENTS));
-  api.get(`${APIRoute.COMMENTS}/${filmId}`)
+  return api.get(`${APIRoute.COMMENTS}/${filmId}`)
     .then(({data}) => dispatch(loadFilmComments(data)))
     .finally(() => dispatch(stopLoading(LoadedData.FILM_COMMENTS)));
 };
 
-
-export const checkAuth = () => (dispatch, _getState, api) => {
+export const checkAuth = () => (dispatch, _getState, api) =>
   api.get(APIRoute.LOGIN)
     .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)))
     .catch(() => {});
-};
 
-export const login = ({login: email, password}) => (dispatch, _getState, api) => {
+export const login = ({login: email, password}) => (dispatch, _getState, api) =>
   api.post(APIRoute.LOGIN, {email, password})
     .then(({data}) => localStorage.setItem('token', data.token))
     .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)))
     .then(() => dispatch(redirectToRoute(AppRoute.MAIN)));
-};
 
-export const logout = () => (dispatch, _getState, api) => {
+export const logout = () => (dispatch, _getState, api) =>
   api.delete(APIRoute.LOGOUT)
     .then(() => localStorage.removeItem('token'))
     .then(() => dispatch(userLogout()));
-};
 
-export const postComment = (filmId, newComment) => (dispatch, _getState, api) => {
+export const postComment = (filmId, newComment) => (dispatch, _getState, api) =>
   api.post(`${APIRoute.COMMENTS}/${filmId}`, newComment)
     .then(({data}) => dispatch(addNewComment(data)))
     .then(() => dispatch(redirectToRoute(`${AppRoute.FILMS}/${filmId}`)))
     .catch(({response}) => dispatch(catchNewCommentError(response.status)));
-};
 
 export const setFilmFavoriteStatus = (filmId, statusId) => (dispatch, _getState, api) => {
   dispatch(startLoading(LoadedData.CURRENT_FILM));
-  api.post(`${APIRoute.FAVORITE}/${filmId}/${statusId}`)
+  return api.post(`${APIRoute.FAVORITE}/${filmId}/${statusId}`)
     .then(({data}) => dispatch(loadCurrentFilm(adaptFilmToClient(data))))
     .finally(() => dispatch(stopLoading(LoadedData.CURRENT_FILM)));
 };
 
 export const setPromotedFilmFavoriteStatus = (filmId, statusId) => (dispatch, _getState, api) => {
   dispatch(startLoading(LoadedData.PROMOTED_FILM));
-  api.post(`${APIRoute.FAVORITE}/${filmId}/${statusId}`)
+  return api.post(`${APIRoute.FAVORITE}/${filmId}/${statusId}`)
     .then(({data}) => dispatch(loadPromotedFilm(adaptFilmToClient(data))))
     .finally(() => dispatch(stopLoading(LoadedData.PROMOTED_FILM)));
 };
