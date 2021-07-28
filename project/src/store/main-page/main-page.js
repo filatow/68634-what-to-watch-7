76713090b60274f21f7abb287e1707e2e30e7c38
evgeniy <not-found@inbox.ru>
2021-07-28 15:05:16 +0300

    Правки по критерию Д16

--- project/src/store/main-page/main-page.js
+++ project/src/store/main-page/main-page.js
@@ -6,20 +6,15 @@ const initialState = {
   films: [],
   promotedFilm: {},
   filterCategory: FilmCategory.ALL_GENRES,
-  filteredFilms: [],
 };
 
 const mainPage = createReducer(initialState, (builder) => {
   builder
     .addCase(loadFilms, (state, action) => {
       state.films = action.payload;
-      state.filteredFilms = action.payload;
     })
     .addCase(setFilterCategory, (state, action) => {
       state.filterCategory = action.payload;
-      state.filteredFilms = (action.payload === FilmCategory.ALL_GENRES)
-        ? state.films
-        : state.films.filter((film) => film.genre === action.payload);
     })
     .addCase(loadPromotedFilm, (state, action) => {
       state.promotedFilm = action.payload;
--- project/src/store/main-page/selectors.js
+++ project/src/store/main-page/selectors.js
@@ -1,6 +1,14 @@
 import {NameSpace} from '../root-reducer';
+import {FilmCategory} from '../../consts';
 
 export const getFilms = (state) => state[NameSpace.MAIN].films;
 export const getFilterCategory = (state) => state[NameSpace.MAIN].filterCategory;
-export const getFilteredFilms = (state) => state[NameSpace.MAIN].filteredFilms;
+export const getFilteredFilms = (state) => {
+  if (state[NameSpace.MAIN].filterCategory === FilmCategory.ALL_GENRES) {
+    return state[NameSpace.MAIN].films;
+  }
+  return state[NameSpace.MAIN].films.filter(
+    (film) => film.genre === state[NameSpace.MAIN].filterCategory);
+};
+
 export const getPromotedFilm = (state) => state[NameSpace.MAIN].promotedFilm;
