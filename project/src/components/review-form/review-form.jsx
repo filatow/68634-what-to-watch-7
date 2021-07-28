d9ff2b088ba6254479ba57febee74f3e78bf4e1f
evgeniy <not-found@inbox.ru>
2021-07-28 14:21:55 +0300

    Правки по критерию Д2

--- project/src/components/review-form/review-form.jsx
+++ project/src/components/review-form/review-form.jsx
@@ -16,11 +16,11 @@ function ReviewForm({filmId}) {
   const [comment, setComment] = useState('');
   const [isReadyToSubmit, setIsReadyToSubmit] = useState(false);
 
-  function onRatingInputClick(evt) {
+  function handleRatingInputClick(evt) {
     setRating(evt.target.value);
   }
 
-  function onTextareaChange(evt) {
+  function handleTextareaChange(evt) {
     setComment(evt.target.value);
   }
 
@@ -36,7 +36,7 @@ function ReviewForm({filmId}) {
     }
   }, [comment, rating]);
 
-  function onSubmit(evt) {
+  function handleFormSubmit(evt) {
     evt.preventDefault();
 
     if (!isReadyToSubmit) {
@@ -52,38 +52,38 @@ function ReviewForm({filmId}) {
     <form
       action="#"
       className="add-review__form"
-      onSubmit={onSubmit}
+      onSubmit={handleFormSubmit}
     >
       <div className="rating">
         <div className="rating__stars">
-          <input className="rating__input" id="star-10" type="radio" name="rating" value="10" onChange={onRatingInputClick} checked={rating === '10'} />
+          <input className="rating__input" id="star-10" type="radio" name="rating" value="10" onChange={handleRatingInputClick} checked={rating === '10'} />
           <label className="rating__label" htmlFor="star-10">Rating 10</label>
 
-          <input className="rating__input" id="star-9" type="radio" name="rating" value="9" onChange={onRatingInputClick} checked={rating === '9'} />
+          <input className="rating__input" id="star-9" type="radio" name="rating" value="9" onChange={handleRatingInputClick} checked={rating === '9'} />
           <label className="rating__label" htmlFor="star-9">Rating 9</label>
 
-          <input className="rating__input" id="star-8" type="radio" name="rating" value="8" onChange={onRatingInputClick} checked={rating === '8'} />
+          <input className="rating__input" id="star-8" type="radio" name="rating" value="8" onChange={handleRatingInputClick} checked={rating === '8'} />
           <label className="rating__label" htmlFor="star-8">Rating 8</label>
 
-          <input className="rating__input" id="star-7" type="radio" name="rating" value="7" onChange={onRatingInputClick} checked={rating === '7'} />
+          <input className="rating__input" id="star-7" type="radio" name="rating" value="7" onChange={handleRatingInputClick} checked={rating === '7'} />
           <label className="rating__label" htmlFor="star-7">Rating 7</label>
 
-          <input className="rating__input" id="star-6" type="radio" name="rating" value="6" onChange={onRatingInputClick} checked={rating === '6'} />
+          <input className="rating__input" id="star-6" type="radio" name="rating" value="6" onChange={handleRatingInputClick} checked={rating === '6'} />
           <label className="rating__label" htmlFor="star-6">Rating 6</label>
 
-          <input className="rating__input" id="star-5" type="radio" name="rating" value="5" onChange={onRatingInputClick} checked={rating === '5'} />
+          <input className="rating__input" id="star-5" type="radio" name="rating" value="5" onChange={handleRatingInputClick} checked={rating === '5'} />
           <label className="rating__label" htmlFor="star-5">Rating 5</label>
 
-          <input className="rating__input" id="star-4" type="radio" name="rating" value="4" onChange={onRatingInputClick} checked={rating === '4'} />
+          <input className="rating__input" id="star-4" type="radio" name="rating" value="4" onChange={handleRatingInputClick} checked={rating === '4'} />
           <label className="rating__label" htmlFor="star-4">Rating 4</label>
 
-          <input className="rating__input" id="star-3" type="radio" name="rating" value="3" onChange={onRatingInputClick} checked={rating === '3'} />
+          <input className="rating__input" id="star-3" type="radio" name="rating" value="3" onChange={handleRatingInputClick} checked={rating === '3'} />
           <label className="rating__label" htmlFor="star-3">Rating 3</label>
 
-          <input className="rating__input" id="star-2" type="radio" name="rating" value="2" onChange={onRatingInputClick} checked={rating === '2'} />
+          <input className="rating__input" id="star-2" type="radio" name="rating" value="2" onChange={handleRatingInputClick} checked={rating === '2'} />
           <label className="rating__label" htmlFor="star-2">Rating 2</label>
 
-          <input className="rating__input" id="star-1" type="radio" name="rating" value="1" onChange={onRatingInputClick} checked={rating === '1'} />
+          <input className="rating__input" id="star-1" type="radio" name="rating" value="1" onChange={handleRatingInputClick} checked={rating === '1'} />
           <label className="rating__label" htmlFor="star-1">Rating 1</label>
         </div>
       </div>
@@ -94,7 +94,7 @@ function ReviewForm({filmId}) {
           name="review-text"
           id="review-text"
           placeholder="Review text"
-          onChange={onTextareaChange}
+          onChange={handleTextareaChange}
           value={comment}
         />
         <div className="add-review__submit">
